e84b9195430c130621bd3f80087b2ea9e91d3941
Robert Crimi <Robert.Crimi@colorado.edu>
2013-10-15 15:06:10 -0700

    final update

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,22 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.find(:all, :order => params[:order])
+    @all_ratings = Movie.all_ratings
+    if !session.has_key?(:ratings) #check if there is a cookie and if not set up so checkboxes are checked
+      session[:ratings] = {"G" => 1, "PG" => 1, "PG-13" => 1, "R" => 1}
+    else
+      if params.has_key?(:ratings) #set session ratings to params ratings
+        session[:ratings] = params[:ratings]
+      end
+    end
+    if !params.has_key?(:ratings) #make sure the params are not empty and set to session
+      params[:ratings] = session[:ratings]
+    end
+    if params.has_key?(:sort_by) #set session order to params order
+      session[:order] = params[:sort_by]
+    end
+
+    @movies = Movie.find_all_by_rating(session[:ratings].keys, :order => session[:order])
   end
 
   def new
